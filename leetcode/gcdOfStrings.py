50dcbb4e86c8da79ba89b72e168edcffca08fe29
burnbrian <98911252+burnbrian@users.noreply.github.com>
2024-08-20 08:19:42 -0400

    Added gcd of strings

---
+++ leetcode/gcdOfStrings.py
@@ -0,0 +1,42 @@
+from leetcode_runner import LeetCode, TestCase, Args
+from typing import *
+
+problem = '''
+For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+Example 1:
+
+Input: str1 = "ABCABC", str2 = "ABC"
+Output: "ABC"
+
+Example 2:
+
+Input: str1 = "ABABAB", str2 = "ABAB"
+Output: "AB"
+
+Example 3:
+
+Input: str1 = "LEET", str2 = "CODE"
+Output: ""
+
+Constraints:
+
+    1 <= str1.length, str2.length <= 1000
+    str1 and str2 consist of English uppercase letters.
+'''
+
+# https://pypi.org/project/leetcode-runner/
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:
+            return ''
+        elif len(str1) == len(str2):
+            return str1
+        elif len(str1) > len(str2):
+            return self.gcdOfStrings(str1[len(str2):], str2)
+        elif len(str2) > len(str1):
+            return self.gcdOfStrings(str2[len(str1):], str1)
+
+LeetCode(problem, Solution).check()
